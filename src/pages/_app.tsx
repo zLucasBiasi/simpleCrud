b124fb8795b50a730bb3c33ff0450eb34ddc90f8
zLucasBiasi <lucasbiasi2001@gmail.com>
2023-01-10 18:32:05 -0300

    feat: add themes and icons to start the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
 import Global from "../styles/global";
+import { themes } from "../styles/themes";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Global />
-      <Component {...pageProps} />
+      <ThemeProvider theme={themes}>
+        <Global />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
---
+++ src/styles/themes.ts
@@ -0,0 +1,8 @@
+export const themes = {
+  colors: {
+    background: "#0A0A0A",
+    cards: "#383838",
+    white: "#fff",
+    red: "#660000",
+  },
+};
